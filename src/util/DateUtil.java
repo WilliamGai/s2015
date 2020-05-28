a7d6eefc894662dc442f6ecabcf33fc57b93e7e4
gaibaoning <gaibaoning@meituan.com>
2020-05-28 16:40:45 +0800

    测试

--- src/util/DateUtil.java
+++ src/util/DateUtil.java
@@ -7,7 +7,7 @@ public enum DateUtil{
 	INSTANCE;
 	
 	private static String format = "";
-	private static SimpleDateFormat sdf = new SimpleDateFormat("YYYY_MM_DD_HH_mm");
+	private static SimpleDateFormat sdf = new SimpleDateFormat("YYYY-MM-DD HH:mm:SSS");
 	public String getTodayStamp(){
 		return sdf.format(new Date());
 	}
